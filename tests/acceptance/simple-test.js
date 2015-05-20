727a3713082a7c83ae3570396216beac2f6b4883
pangratz <cmueller.418@gmail.com>
2015-05-20 21:41:50 +0200

    Possibility to add babel plugins via registry
    This allows Ember-CLI addons to add custom babel transformations to the
    ember-cli-babel pipeline by registering a plugin using the
    `babel-plugin` type:
    
    ``` javascript
    // my-addon/index.js
    module.exports = {
      name: 'my-addon-using-babel-transformer',
    
      setupPreprocessorRegistry: function(type, registry) {
        var BabelTransformerPlugin = require('./babel-transformer-plugin');
    
        registry.add('babel-plugin', {
          name: 'my-addon-using-babel-transformer',
          plugin: BabelTransformerPlugin
        });
      }
    }
    
    // my-addon/babel-transformer-plugin.js
    module.exports = function(babel) {
      var t = babel.types;
      return new babel.Transformer('troll-transformer', {
        Literal: function(node) {
          if (node.value === true) {
            return t.literal(false);
          } else if (node.value === false) {
            return t.literal(true);
          }
        }
      });
    }
    ```

--- tests/acceptance/simple-test.js
+++ tests/acceptance/simple-test.js
@@ -21,3 +21,10 @@ test('value of input', function(assert) {
     assert.equal('Test Value', find('#test-input').val());
   });
 });
+
+test('babel-plugin is looked up from the registry', function(assert) {
+  // the string below is replaced by a babel transformation, which is
+  // registered as 'babel-plugin' within the 'answer-to-the-universe' in repo
+  // addon
+  assert.equal('THE-answer', 42);
+});
